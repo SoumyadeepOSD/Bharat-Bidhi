6210a23b59721bf639d75ecb852de5ea2baecb7a
SoumyadeepOSD <opencontainer255@gmail.com>
2023-09-23 02:08:00 +0530

    Website prototype complete

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
 import { ChakraProvider } from '@chakra-ui/react'
-import {Header} from './components/header.js';
-import Footer from './components/footer.js';
+import { Header } from './components/header.js';
+import { Footer } from './components/footer.js';
 import Hero from './components/hero.js';
-import {Main} from './components/main.js';
+import { Main } from './components/main.js';
 
-const App=() =>{
+const App = () => {
   return (
-    <div className="App">
+    <div className="App" style={{ position: 'relative' }}>
       <ChakraProvider>
-      <Header/>
-      <Hero/>
-      <Main/>
-      <Footer/>
+        <Header />
+        <Hero />
+        <Main />
+        <Footer />
+        <div
+        style={{
+          position: 'fixed',
+          bottom: 0,
+          right: 0,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '100%',
+        }}
+      >
+        <img
+          src='https://static.vecteezy.com/system/resources/previews/001/545/992/original/indian-woman-s-face-avatar-free-vector.jpg'
+          height={'100px'}
+          width={'100px'}
+          borderRadius={'100px'}
+          alt="Sticky Image"
+        />
+      </div>
+
       </ChakraProvider>
+      
     </div>
   );
 }
 
 export default App;
-
-
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,71 @@
+/* eslint-disable jsx-a11y/alt-text */
+import {Box, Flex, Text} from '@chakra-ui/react';
+import Carousel from 'react-bootstrap/Carousel';
+
+const logo1 = "https://cdnbbsr.s3waas.gov.in/s35d6646aad9bcc0be55b2c82f69750387/uploads/bfi_thumb/2019052222-pbg4yfykt1ws3wesk2e72qrx16pvhgliyhns4ln6js.png";
+const logo2 = "https://awards.gov.in/Content/images/g20-logo.png";
+const logo3 = "https://cdnbbsr.s3waas.gov.in/s35d6646aad9bcc0be55b2c82f69750387/uploads/bfi_thumb/2019032217-pbg4ynhabq72os3vc5n7movls9ot71fdnivnytc160.png";
+const logo4 = "https://cdnbbsr.s3waas.gov.in/s35d6646aad9bcc0be55b2c82f69750387/uploads/bfi_thumb/2019041050-pbg4yis3dk0n2qap3lm2s82atcbz4jwpyvm8kfj014.png";
+const logo5 = "https://cdnbbsr.s3waas.gov.in/s35d6646aad9bcc0be55b2c82f69750387/uploads/bfi_thumb/2023062719-q8kt1gkmm5c63scixgxrphb60r09yrjv6b3wjgaxuw.png";
+const logo6 = "https://cdnbbsr.s3waas.gov.in/s35d6646aad9bcc0be55b2c82f69750387/uploads/bfi_thumb/2019052265-pbg4yfykt1ws3wesk2e72qrx16pvhgliyhns4ln6js.png";
+const nic = "https://doj.gov.in/wp-content/themes/sdo-theme/images/nicLogo.png";
+const digitalIndia = "https://doj.gov.in/wp-content/themes/sdo-theme/images/digitalIndia.png";
+const swaas = "https://doj.gov.in/wp-content/themes/sdo-theme/images/s3waas.png";
+
 const Footer = ()=>{
     return(
-        <div>
-            Footer
-        </div>
+        <Flex background={'black'} width={'100%'} height={'300px'} flexDirection={'column'} alignItems={'center'}> 
+           <Flex flexDirection={'row'} justifyContent={'space-between'} alignItems={'center'}>
+           <img  src={logo1} width={'100%'}/>
+           <img  src={logo2} width={'180px'}/>
+           <img  src={logo3} width={'100%'}/>
+           <img  src={logo4} width={'100%'}/>
+           <img  src={logo5} width={'100%'}/>
+           <img  src={logo6} width={'100%'}/>
+           </Flex>
+           <br/>
+           <br/>
+           <hr style={{color: 'white', height: '4px'}}/>
+           <Flex flexDirection={'row'} alignItems={'center'}  gap={'30px'}>
+                <img src={swaas} height={'150px'} width={'150px'}/>
+                <img src={nic} height={'150px'} width={'150px'}/>
+                <img src={digitalIndia} height={'150px'} width={'150px'}/>
+            </Flex>
+        </Flex>
     );
 }
 
-export default Footer;
+
+const FooterCuroselSlider = () =>{
+    return (
+        <Carousel interval={2000} width={'100%'}>
+      <Carousel.Item>
+      <img  src={logo1} width={'100%'}/>
+      </Carousel.Item>
+
+
+      <Carousel.Item>
+      <img  src={logo3} width={'100%'}/>
+      </Carousel.Item>
+
+      <Carousel.Item>
+      <img  src={logo4} width={'100%'}/>
+      </Carousel.Item>
+
+
+      <Carousel.Item>
+      <img  src={logo5} width={'100%'}/>
+      </Carousel.Item>
+
+      
+      <Carousel.Item>
+      <img  src={logo6} width={'100%'}/>
+      </Carousel.Item>
+     
+    </Carousel>
+    );
+  }
+
+
+
+  export {Footer, FooterCuroselSlider};
