5926271f6f1469a6e878fe0ee21d8baa4a74c18d
SoumyadeepOSD <opencontainer255@gmail.com>
2023-09-23 02:17:08 +0530

    Favicon and title changed

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+document.title = 'Bharat विधि';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
